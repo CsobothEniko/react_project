4e2fbb6a0679ffdef435f156ca29f541cec7644d
CsobothEniko <enikc016@gmail.com>
2021-05-14 15:43:52 +0200

    create the first page with login link

---
+++ team_maker/src/components/FirstPage.js
@@ -0,0 +1,22 @@
+import { Login } from './Login';
+import { GlobalProvider } from '../context/GlobalState';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+export const First_page = () => {
+    return (
+        <div style={{ maxWidth: "17rem", margin: "4rem auto" }}>
+            <h1> Welcome guy! </h1>
+              <GlobalProvider>
+                  <Router>
+                    <Switch>
+                        <Route exact path="/login" component={Login} />
+                    </Switch>
+                  </Router>
+              </GlobalProvider>
+              <div className ="text-center">
+              <Link to="/login" className="btn btn-danger"> Login</Link>
+              </div>
+        </div>
+    )
+}
