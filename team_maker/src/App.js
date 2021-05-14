bc4ef225205a1af7fb178093004f6e51375d351d
CsobothEniko <enikc016@gmail.com>
2021-05-14 16:20:12 +0200

    deleting the css and create the routes in app.js

--- team_maker/src/App.js
+++ team_maker/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route , Link} from 'react-router-dom';
+import { Home } from './components/Home';
+import { Login } from './components/Login';
+import { AddCharacter } from './components/AddCharacter';
+import { Edit } from './components/Edit';
+import { FirstPage } from './components/FirstPage';
+import { GlobalProvider } from './context/GlobalState';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div style={{ maxWidth: "60rem", margin: "4rem auto" }}>
+              <GlobalProvider>
+                  <Router>
+                    <Switch>
+                        <Route exact path="/" component={FirstPage} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/home" component={Home} />
+                        <Route path="/add" component={AddCharacter} />
+                        <Route path="/edit/:id" component={Edit} />
+                    </Switch>
+                  </Router>
+              </GlobalProvider>
+        </div>
+    );
 }
 
 export default App;
