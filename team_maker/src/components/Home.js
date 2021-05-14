0ae4848a98f504e92d9e364e0da706ca921cf1f9
CsobothEniko <enikc016@gmail.com>
2021-05-14 16:00:54 +0200

    create the home page

---
+++ team_maker/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Head } from './Head';
+import { List } from './List';
+
+export const Home = () => {
+    return (
+        <>
+            <Head />
+            <List />
+        </>
+    )
+}
